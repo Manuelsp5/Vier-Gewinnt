654d98904dfd98b0d4ded9d6f9407dab8804821a
Damian Max Götze <4188@htl.rennweg.at>
2026-06-08 11:02:05 +0200

    JavaFx eingebettet

--- src/startmenu.java
+++ src/startmenu.java
@@ -1,12 +1,20 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
-public class startmenu extends Application{
+
+public class startmenu extends Application {
+
     @Override
     public void start(Stage stage) {
-
+        Label l = new Label("JavaFX funktioniert!");
+        Scene scene = new Scene(l, 300, 200);
+        stage.setScene(scene);
+        stage.setTitle("Test");
+        stage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
