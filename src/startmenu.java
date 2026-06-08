cd00a2a1a61140a134ab6c3ed05a627d00353445
Manuel <4204@htl.rennweg.at>
2026-06-08 10:29:14 +0200

    Test

---
+++ src/startmenu.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+public class startmenu extends Application{
+    @Override
+    public void start(Stage stage) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
